7ba6247cceb6f2736ab2d6178f0c210e223b61db
Pietro Nencini <nencinipietro2006@gmail.com>
2024-10-23 16:22:10 +0200

    Fixedl all issues - end exercise

--- script.php
+++ script.php
@@ -5,21 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Esercizio 26</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="styles.css" type="text/css">
+    <link rel="stylesheet" type="text/css" href="styles.css">
 </head>
 <body>
-    
     <?php
-
         $name = $_GET["name"];
         $surname = $_GET["surname"];
         $birthdate = $_GET["birthdate"];
         $home_time = $_GET["home_time"];
-        $how_transport = $_GET["transport"];         
         if(!isset($how_transport)) {
-            echo "Devi selezionare almeno un modo di trasporto";
+            echo "<p class='text-danger'>Devi selezionare almeno un modo di trasporto</p>";
             exit();
-        }
+        } else
+            $how_transport = $_GET["transport"];
         if(!isset($_GET["english"]) && !isset($_GET["teathre"]) && !isset($_GET["sport"])) {
             $extra_school = "Nessuna attività extrascolastica";
         } else {
@@ -36,25 +34,27 @@
             $extra_school = rtrim($extra_school, ", ");
         }
     ?>
-    <table class="p-4">
-        <tr>
-            <th> Nome </th>
-            <th> Cognome </th>
-            <th> Data di Nascita </th>
-            <th> Ora di arrivo a casa* </th>
-            <th> Come viene a scuola </th>
-            <th> Attività extrascolastiche frequentate </th>
-        </tr>
-        <tr>
-            <td> <?php $name ?></td>
-            <td> <?php $surname ?></td>
-            <td> <?php $birthdate ?></td>
-            <td> <?php $home_time ?></td>
-            <td> <?php $how_transport ?></td>
-            <td> <?php $extra_school ?></td>
-        </tr>
-    </table>
-    <p >*si intende nei giorni di maggiore durata scolastica (ora di uscita: 14:40)</p>
+    <div class="w-100">
+        <table class="d-block mx-auto p-4" id="data_table">
+            <tr>
+                <th> Nome </th>
+                <th> Cognome </th>
+                <th> Data di Nascita </th>
+                <th> Ora di arrivo a casa* </th>
+                <th> Come viene a scuola </th>
+                <th> Attività extrascolastiche frequentate </th>
+            </tr>
+            <tr>
+                <td> <?php echo $name ?></td>
+                <td> <?php echo $surname ?></td>
+                <td> <?php echo $birthdate ?></td>
+                <td> <?php echo $home_time ?></td>
+                <td> <?php echo $how_transport ?></td>
+                <td> <?php echo $extra_school ?></td>
+            </tr>
+        </table>
+        <p class="my-4">*si intende nei giorni di maggiore durata scolastica (ora di uscita: 14:40)</p>
+    </div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
